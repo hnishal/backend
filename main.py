a9a721cb7146296a78affdd5d074416d625cd2c3
gottabpro <sj931293@gmail.com>
2021-03-23 23:37:38 +0530

    Second commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 from flask import Flask, jsonify, request
 import requests
 import pymongo
-from bid import Bid,create_bid
+from bid import Bid, create_bid
 from project import Project, add_project
 from profile import Profile, create_profile
 
@@ -16,48 +16,49 @@ bids = my_db['bids']
 
 # my_db.user_data.drop()
 
-users.create_index("user_id",unique = True)
-users.create_index("username",unique = True)
-users.create_index("email",unique = True)
-projects.create_index("project_id",unique = True)
-bids.create_index("bid_id",unique = True)
-
+users.create_index("user_id", unique=True)
+users.create_index("username", unique=True)
+users.create_index("email", unique=True)
+projects.create_index("project_id", unique=True)
+bids.create_index("bid_id", unique=True)
 
 
 app = Flask(__name__)
 
-@ app.route('/api/create_profile',methods=['POST'])
-def new_profile():
-    json_request=request.get_json()
-    new_profile=create_profile(json_request['profile'])
-    response = str(users.insert_one(new_profile.__dict__))
-    return jsonify(response),201
-    
 
-@ app.route('/api/post_project',methods=['POST'])
+@ app.route('/api/create_profile', methods=['POST'])
+def new_profile():
+    json_request = request.get_json()
+    new_profile = create_profile(json_request['profile'])
+    response = str(users.insert_one(new_profile.__dict__))
+    return jsonify(response), 201
+
+
+@ app.route('/api/post_project', methods=['POST'])
 def post_project():
-    json=request.get_json()
-    new_project=add_project(json['project'])
-    user=users.find_one({"user_id": new_project.user_id})
-    project_array=user["projects"]
+    json = request.get_json()
+    new_project = add_project(json['project'])
+    user = users.find_one({"user_id": new_project.user_id})
+    project_array = user["projects"]
     project_array.append(new_project.project_id)
-    newvalues={"$set":{"projects":project_array}}
+    newvalues = {"$set": {"projects": project_array}}
     users.update_one({"user_id": new_project.user_id}, newvalues)
     response = str(projects.insert_one(new_project.__dict__))
-    return jsonify(response),201
+    return jsonify(response), 201
 
-# @ app.route('/api/place_bid',methods=['POST'])
-# def create_new_bid():
-#     json=request.get_json()
-#     new_bid=create_bid(json['bid'])
-#     myquery= {"id": new_bid.project_id}
-#     mydoc=mycol2.find_one(myquery)
-#     new_bids=mydoc["bids"]
-#     new_bids.append(new_bid.__dict__)
-#     newvalues={"$set":{"bids":new_bids}}
-#     mycol2.update_one(myquery, newvalues)
-#     response ="success"
-#     return jsonify(response),201
+
+@ app.route('/api/place_bid', methods=['POST'])
+def create_new_bid():
+    json = request.get_json()
+    new_bid = create_bid(json['bid'])
+    myquery = {"project_id": new_bid.project_id}
+    mydoc = projects.find_one(myquery)
+    new_bids = mydoc["bids"]
+    new_bids.append(new_bid.__dict__)
+    newvalues = {"$set": {"bids": new_bids}}
+    projects.update_one(myquery, newvalues)
+    response = "success"
+    return jsonify(response), 201
 
 # @ app.route('/api/search_project/<int:id>',methods=['GET'])
 # def search_project(id):
@@ -83,5 +84,4 @@ def post_project():
 # @ app.route('/api/get_projects',methods=['POST'])
 
 
-app.run(host='0.0.0.0',port=5000)
-    
+app.run(host='0.0.0.0', port=5000)
--- project.py
+++ project.py
@@ -8,38 +8,38 @@ import enum
 import random
 from bid import Bid, create_bid
 
-count=-1
+count = -1
+
 
 class Project:
-    def __init__(self,project):
-        self.name=project['name']
-        self.project_id=None
-        self.description=project['description']
-        self.skills=project['skills']
-        self.status=1
-        self.bids=[]
-        self.user_id=project['user_id']
-        #add timestamp, file upload
-    
-    
+    def __init__(self, project):
+        self.name = project['name']
+        self.project_id = None
+        self.description = project['description']
+        self.skills = project['skills']
+        self.status = 1
+        self.bids = []
+        self.user_id = project['user_id']
+        # add timestamp, file upload
+
     def close_project(self):
-        self.status=0
-    
+        self.status = 0
+
     def reopen_project(self):
-        self.status=1
+        self.status = 1
 
     def get_status(self):
         return self.status
-    
-    def add_bid(self,bid):
-        new_bid=create_bid(bid)
+
+    def add_bid(self, bid):
+        new_bid = create_bid(bid)
         self.bids.append(new_bid)
         return 'success1'
-    
+
 
 def add_project(project):
     global count
-    count+=1
-    new_project=Project(project)
-    new_project.project_id=count
+    count += 1
+    new_project = Project(project)
+    new_project.project_id = count
     return new_project
