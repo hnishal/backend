1bb2278fd51707a3529e095666bca30741c75ad8
Himanshu Nishal <hnishal12799@gmail.com>
2021-03-24 01:03:03 +0530

    routes implemented

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 from flask import Flask, jsonify, request
 import requests
 import pymongo
+from bson import json_util, ObjectId
 from bid import Bid, create_bid
 from project import Project, add_project
 from profile import Profile, create_profile
@@ -54,34 +55,57 @@ def create_new_bid():
     myquery = {"project_id": new_bid.project_id}
     mydoc = projects.find_one(myquery)
     new_bids = mydoc["bids"]
-    new_bids.append(new_bid.__dict__)
+    new_bids.append(new_bid.bid_id)
     newvalues = {"$set": {"bids": new_bids}}
     projects.update_one(myquery, newvalues)
     response = "success"
     return jsonify(response), 201
 
-# @ app.route('/api/search_project/<int:id>',methods=['GET'])
-# def search_project(id):
-#     myquery= {"id": id}
-#     mydoc=mycol2.find_one(myquery)
-#     return jsonify(str(mydoc)),201
+@ app.route('/api/search_project/<int:id>',methods=['GET'])
+def search_project(id):
+    myquery= {"project_id": id}
+    mydoc=projects.find_one(myquery)
+    return jsonify(str(mydoc)),201
 
-# @ app.route('/api/close_project/<int:id>',methods=['PUT'])
-# def close_project(id):
-#     myquery= {"id": id}
-#     # mydoc=mycol2.find_one(myquery)
-#     newvalues={"$set":{"status":0}}
-#     mycol2.update_one(myquery, newvalues)
-#     response ="success"
-#     return jsonify(response),201
+@ app.route('/api/close_project/<int:id>',methods=['PUT'])
+def close_Project(id):
+    myquery= {"project_id": id}
+    # mydoc=mycol2.find_one(myquery)
+    newvalues={"$set":{"status":0}}
+    projects.update_one(myquery, newvalues)
+    response ="success"
+    return jsonify(response),201
 
-# @ app.route('/api/get_proposals',methods=['POST'])
+@ app.route('/api/get_bids/<int:id>',methods=['PUT'])
+def show_bids(id):
+    myquery={"project_id":id}
+    temp=projects.find_one(myquery)
+    return jsonify(temp["bids"]),temp
 
-# @ app.route('/api/view_profile',methods=['GET'])
 
-# @ app.route('/api/get_my_projects',methods=['POST'])
+@ app.route('/api/view_profile/<int:id>',methods=['GET'])
+def show_profile(id):
+    myquery= {"user_id": id}
+    temp=users.find_one(myquery)
+    return json_util.dumps(temp),201
 
-# @ app.route('/api/get_projects',methods=['POST'])
+@ app.route('/api/get_my_projects/<int:id>',methods=['PUT'])
+def get_my_projects(id):
+    myquery= {"user_id":id}
+    temp= users.find_one(myquery)
+    project_arr=temp["projects"]
+    list1=[]
+    for i in project_arr:
+        new_proj=projects.find_one({"project_id":i})
+        list1.append(json_util.dumps(new_proj))
+    return jsonify(list1)
+
+@ app.route('/api/get_projects',methods=['GET'])
+def get_projects():
+    temp=projects.find()
+    temp=list(temp)
+    temp=json_util.dumps(temp)
+    return temp,201
 
 
 app.run(host='0.0.0.0', port=5000)
--- project.py
+++ project.py
@@ -8,7 +8,7 @@ import enum
 import random
 from bid import Bid, create_bid
 
-count = -1
+count = 0
 
 
 class Project:
@@ -22,20 +22,21 @@ class Project:
         self.user_id = project['user_id']
         # add timestamp, file upload
 
-    def close_project(self):
-        self.status = 0
+    
 
     def reopen_project(self):
         self.status = 1
-
-    def get_status(self):
-        return self.status
 
     def add_bid(self, bid):
         new_bid = create_bid(bid)
         self.bids.append(new_bid)
         return 'success1'
 
+def close_project(project):
+        project["status"] = 0
+
+def get_status(project):
+        return project["status"]
 
 def add_project(project):
     global count
